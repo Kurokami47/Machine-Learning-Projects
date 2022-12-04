b894ab834c71d99b5fcef40102e0daeb2dc959d6
Jagannath Padhy <53792412+Kurokami47@users.noreply.github.com>
2022-12-04 13:24:39 +0530

    added artifact

---
+++ housing/pipeline/pipeline.py
@@ -0,0 +1,6 @@
+from housing.config.configuration import Configuration
+from housing.logger import logging
+from housing.exception import housingexception
+
+from housing.entity.artifact_entity import DataIngestionArtifact
+from housing.entity.config_entity import DataIngestionConfig
